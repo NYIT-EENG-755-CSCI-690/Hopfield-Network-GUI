1101c7dd48df8f314e23da3ea8ee5af4b07ebfe0
Albert Chen <albert.chen0329@gmail.com>
2023-03-27 22:28:21 -0400

    Update Styles with flexbox

--- TermProjectRepo/src/BallContainer.jsx
+++ TermProjectRepo/src/BallContainer.jsx
@@ -4,16 +4,16 @@ import preload from "./data.json";
 // TODO: breakdown container into ball components, apply styling to each component
 
 const BallContainer = () => (
-    <div className="balls">
+    <div className="balls-container">
         {preload.balls.map((ball, i) => (
-            <div>
-                <div className="ball" key={i} style={{ "--i": i }}>
+            <div className="ball-set">
+                <div className="ball-one" key={i} style={{ "--i": i }}>
                     {ball.val_one}
                 </div>
-                <div className="ball" key={i} style={{ "--i": i }}>
+                <div className="ball-two" key={i} style={{ "--i": i }}>
                     {ball.val_two}
                 </div>
-                <div className="ball" key={i} style={{ "--i": i }}>
+                <div className="ball-three" key={i} style={{ "--i": i }}>
                     {ball.val_three}
                 </div>
             </div>
