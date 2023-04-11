04e442837ecaa6c4dce608f75f308bfab98c2a74
Albert Chen <albert.chen0329@gmail.com>
2023-04-11 16:59:47 -0400

    Connect Frontend to Backend via proxy server

--- TermProjectRepo/src/App.jsx
+++ TermProjectRepo/src/App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom";
 import Landing from "./Landing";
 import BallContainer from "./BallContainer";
 
-const App = () => (
-    <div className="app">
-        <div className="landing">
-            <Landing />
-            <BallContainer />
+// TODO: replace dummy data. Migrate ball dataset to api server and map data onto landing page.
+
+const App = () => {
+    const [currentTime, setCurrentTime] = useState(1);
+
+    useEffect(() => {
+        fetch("/time")
+            .then((res) => res.json())
+            .then((data) => {
+                setCurrentTime(data);
+            });
+    }, []);
+
+    return (
+        <div className="app">
+            <div className="landing">
+                <Landing />
+                <BallContainer />
+                <div>The current time is {currentTime.time} </div>.
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
--- TermProjectRepo/vite.config.js
+++ TermProjectRepo/vite.config.js
@@ -1,4 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({ plugins: [react()], root: "src" });
+export default defineConfig({
+    server: {
+        proxy: {
+            "/time": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+    plugins: [react()],
+    root: "src",
+});
