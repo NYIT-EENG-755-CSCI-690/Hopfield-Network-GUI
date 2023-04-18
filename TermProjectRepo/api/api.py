557232d51a965204ab3e00df5eec8d196f8db7a8
Albert Chen <albert.chen0329@gmail.com>
2023-04-18 14:40:43 -0400

    HNN algorithm integrated into API Route

--- TermProjectRepo/api/api.py
+++ TermProjectRepo/api/api.py
@@ -1,5 +1,9 @@
 import time
-from flask import Flask
+from HopfieldNetwork import Text_Processor 
+from flask import Flask, request
+
+default_words = ['BEANS', 'APPLE', 'VALID', 'STACK']
+txt_processor = Text_Processor(default_words)
 
 
 app = Flask(__name__)
@@ -7,3 +11,21 @@ app = Flask(__name__)
 @app.route('/time')
 def get_current_time():
     return {"time": 22}
+
+
+@app.route('/set-words', methods=['PUT'])
+def set_words_list():
+    pass
+    
+@app.route('/process-word', methods=['GET'])
+def process_word():
+    # args = request.args
+    # word = args['word']
+    # global txt_processor
+    word_pair = txt_processor.process_word('BEAMZ')
+    input_word = word_pair[0]
+    output_word = word_pair[1]
+    return {
+        "input word": input_word,
+        "output word": output_word
+        }
